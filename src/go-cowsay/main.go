84d81311c5538a74388acff344fd511027c6d490
Matt Smith <matthew.smith491@gmail.com>
2016-12-08 13:35:37 -0700

    Adding stdin reading and fixing small bubble creation bug

--- src/go-cowsay/main.go
+++ src/go-cowsay/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -15,10 +17,19 @@ var (
 
 func main() {
 	flag.Parse()
+	var message string
+	fi, _ := os.Stdin.Stat()
+	if fi.Size() > 0 {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		message = makeBubble(strings.TrimSpace(text))
+	} else {
+		message = makeBubble(strings.Join(flag.Args(), " "))
+	}
 	// fmt.Println(flag.Args())
 	// splitArgs := strings.Split(strings.Join(flag.Args(), " "), "-")
 	// firstArgs := splitArgs[:len(splitArgs)]
-	message := makeBubble(strings.Join(flag.Args(), " "))
+
 	data, err := Asset(fmt.Sprintf("src/go-cowsay/cows/%s.cow", *whichCow))
 	if err != nil {
 		fmt.Println("Couldn't access asset")
@@ -57,10 +68,10 @@ func makeBubble(s string) string {
 		b.WriteString(s[:index+40])
 		b.WriteString(pad + "\\\n")
 		for true {
+			index += 40
 			if len(s) < index+40 {
 				break
 			}
-			index += 40
 			b.WriteString("|" + pad)
 			if s[index+40] == ' ' {
 				b.WriteString(s[index : index+40])
@@ -82,7 +93,7 @@ func makeBubble(s string) string {
 			b.WriteString(pad + "|\n")
 		}
 		b.WriteString("\\" + pad)
-		b.WriteString(s[index:])
+		b.WriteString(strings.TrimSpace(s[index:]))
 		for i := 0; i < 40-(len(s)-index); i++ {
 			b.WriteString(pad)
 		}
